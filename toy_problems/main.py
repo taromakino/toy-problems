35f96d9dede3f7051cd6bba1c7e68dde7703758c
makinot1 <makinot1@gene.com>
2023-12-28 08:24:24 -0800

    Remove dropout

--- toy_problems/main.py
+++ toy_problems/main.py
@@ -36,8 +36,8 @@ def make_model(args, task, eval_stage):
         else:
             return ERM.load_from_checkpoint(ckpt_fpath(args, task))
     elif task == Task.VAE:
-        return VAE(task, args.z_size, args.rank, args.h_sizes, args.y_mult, args.beta, args.dropout_prob, args.reg_mult,
-            args.init_sd, args.lr, args.weight_decay, args.lr_infer, args.n_infer_steps)
+        return VAE(task, args.z_size, args.rank, args.h_sizes, args.y_mult, args.beta, args.reg_mult, args.init_sd,
+            args.lr, args.weight_decay, args.lr_infer, args.n_infer_steps)
     else:
         assert task == Task.CLASSIFY
         return VAE.load_from_checkpoint(ckpt_fpath(args, Task.VAE), task=task)
@@ -106,7 +106,6 @@ if __name__ == '__main__':
     parser.add_argument('--h_sizes', nargs='+', type=int, default=[256, 256])
     parser.add_argument('--y_mult', type=float, default=1)
     parser.add_argument('--beta', type=float, default=1)
-    parser.add_argument('--dropout_prob', type=float, default=0.1)
     parser.add_argument('--reg_mult', type=float, default=1e-5)
     parser.add_argument('--init_sd', type=float, default=1e-3)
     parser.add_argument('--lr', type=float, default=1e-3)
