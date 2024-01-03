Bug fix, causal posterior should not condition on e

toy_problems/vae.py
@@ -17,22 +17,21 @@ class Encoder(nn.Module):
         self.causal_size = causal_size
         self.spurious_size = spurious_size
         self.encoder_cnn_causal = EncoderCNN()
-        self.mu_causal = SkipMLP(IMG_ENCODE_SIZE + N_ENVS, h_sizes, causal_size)
-        self.offdiag_causal = SkipMLP(IMG_ENCODE_SIZE + N_ENVS, h_sizes, causal_size ** 2)
-        self.diag_causal = SkipMLP(IMG_ENCODE_SIZE + N_ENVS, h_sizes, causal_size)
+        self.mu_causal = SkipMLP(IMG_ENCODE_SIZE, h_sizes, causal_size)
+        self.offdiag_causal = SkipMLP(IMG_ENCODE_SIZE, h_sizes, causal_size ** 2)
+        self.diag_causal = SkipMLP(IMG_ENCODE_SIZE, h_sizes, causal_size)
         self.encoder_cnn_spurious = EncoderCNN()
         self.mu_spurious = SkipMLP(IMG_ENCODE_SIZE + N_CLASSES + N_ENVS, h_sizes, spurious_size)
         self.offdiag_spurious = SkipMLP(IMG_ENCODE_SIZE + N_CLASSES + N_ENVS, h_sizes, spurious_size ** 2)
         self.diag_spurious = SkipMLP(IMG_ENCODE_SIZE + N_CLASSES + N_ENVS, h_sizes, spurious_size)
 
-    def causal_dist(self, x, e):
+    def causal_dist(self, x):
         batch_size = len(x)
         x = self.encoder_cnn_causal(x).view(batch_size, -1)
-        e_one_hot = one_hot(e, N_ENVS)
-        mu = self.mu_causal(x, e_one_hot)
-        offdiag = self.offdiag_causal(x, e_one_hot)
+        mu = self.mu_causal(x)
+        offdiag = self.offdiag_causal(x)
         offdiag = offdiag.reshape(batch_size, self.causal_size, self.causal_size)
-        diag = self.diag_causal(x, e_one_hot)
+        diag = self.diag_causal(x)
         cov = arr_to_cov(offdiag, diag)
         return D.MultivariateNormal(mu, cov)
 
@@ -49,7 +48,7 @@ class Encoder(nn.Module):
         return D.MultivariateNormal(mu, cov)
 
     def forward(self, x, y, e):
-        causal_dist = self.causal_dist(x, e)
+        causal_dist = self.causal_dist(x)
         spurious_dist = self.spurious_dist(x, y, e)
         return causal_dist, spurious_dist
 