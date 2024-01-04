c5932ac6565522da66219ae95ce57b36681c656b
makinot1 <makinot1@gene.com>
2024-01-03 20:33:58 -0800

    Remove edge from e to z_c

--- toy_problems/data/dsprites.py
+++ toy_problems/data/dsprites.py
@@ -5,7 +5,6 @@ from utils.nn_utils import make_dataloader
 
 
 RNG = np.random.RandomState(0)
-PROB_ZERO_E0 = 0.2
 N_TRAINVAL = 10000
 N_TEST = 2000
 WIDTH_SMALL = 8
@@ -25,12 +24,7 @@ def make_trainval_data():
 
     y = flip_binary(scale.copy(), 0.25)
 
-    idxs_env0 = []
-    zero_idxs = np.where(scale == 0)[0]
-    one_idxs = np.where(scale == 1)[0]
-    idxs_env0.append(RNG.choice(zero_idxs, size=int(PROB_ZERO_E0 * len(zero_idxs))))
-    idxs_env0.append(RNG.choice(one_idxs, size=int((1 - PROB_ZERO_E0) * len(one_idxs))))
-    idxs_env0 = np.concatenate(idxs_env0)
+    idxs_env0 = RNG.choice(N_TRAINVAL, N_TRAINVAL // 2, replace=False)
     idxs_env1 = np.setdiff1d(np.arange(N_TRAINVAL), idxs_env0)
 
     e = np.zeros(N_TRAINVAL, dtype='long')
@@ -65,6 +59,9 @@ def make_trainval_data():
     x[:, 0, :, :] *= colors
     x[:, 1, :, :] *= (1 - colors)
     x = torch.tensor(x, dtype=torch.float32)
+
+    x[idxs_env0, 0, 0, 0] = 1
+    x[idxs_env1, 1, 0, 0] = 1
 
     y = torch.tensor(y, dtype=torch.long)
     e = torch.tensor(e, dtype=torch.long)
@@ -124,6 +121,10 @@ def make_data(train_ratio, batch_size, eval_batch_size):
 def main():
     x, y, e, c, s = make_trainval_data()
     color = s
+    fig, ax = plt.subplots(1, 1, figsize=(6, 3))
+    ax.hist(color, bins='auto')
+    ax.set_title('p(color)')
+    fig.tight_layout()
     fig, axes = plt.subplots(1, 2, figsize=(6, 3))
     axes[0].hist(color[(y == 0) & (e == 0)], bins='auto', alpha=0.5, color='red')
     axes[0].hist(color[(y == 1) & (e == 0)], bins='auto', alpha=0.5, color='blue')
