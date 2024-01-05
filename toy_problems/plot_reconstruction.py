7b9c4c9e64bf7faec34564e270f2ffa7b56e1287
makinot1 <makinot1@gene.com>
2024-01-05 10:28:49 -0800

    Incorporate zx in plot_reconstruction

--- toy_problems/plot_reconstruction.py
+++ toy_problems/plot_reconstruction.py
@@ -16,9 +16,9 @@ N_COLS = 10
 def sample_prior(rng, model):
     y = torch.tensor(rng.choice(N_CLASSES), dtype=torch.long, device=model.device)[None]
     e = torch.tensor(rng.choice(N_ENVS), dtype=torch.long, device=model.device)[None]
-    causal_dist, spurious_dist = model.prior(y, e)
-    zc_sample, zs_sample = causal_dist.sample(), spurious_dist.sample()
-    return zc_sample, zs_sample
+    prior_causal, prior_spurious, prior_exogenous = model.prior(y, e)
+    zc_sample, zs_sample, zx_sample = prior_causal.sample(), prior_spurious.sample(), prior_exogenous.sample()
+    return zc_sample, zs_sample, zx_sample
 
 
 def reconstruct_x(model, z):
@@ -39,25 +39,23 @@ def main(args):
     for example_idx in range(N_EXAMPLES):
         x_seed, y_seed, e_seed = x[[example_idx]], y[[example_idx]], e[[example_idx]]
         x_seed, y_seed, e_seed = x_seed.to(model.device), y_seed.to(model.device), e_seed.to(model.device)
-        causal_dist, spurious_dist = model.encoder(x_seed, y_seed, e_seed)
-        zc_seed, zs_seed = causal_dist.loc, spurious_dist.loc
-        z_seed = torch.hstack((zc_seed, zs_seed))
-        fig, axes = plt.subplots(2, N_COLS, figsize=(2 * N_COLS, 2 * 2))
+        posterior_causal, posterior_spurious, posterior_exogenous = model.encoder(x_seed, y_seed, e_seed)
+        zc_seed, zs_seed, zx_seed = posterior_causal.loc, posterior_spurious.loc, posterior_exogenous.loc
+        fig, axes = plt.subplots(3, N_COLS, figsize=(2 * N_COLS, 2 * 2))
         for ax in axes.flatten():
             ax.set_xticks([])
             ax.set_yticks([])
         plot = PLOT[args.dataset]
         plot(axes[0, 0], x_seed.squeeze().cpu().numpy())
         plot(axes[1, 0], x_seed.squeeze().cpu().numpy())
-        x_pred = reconstruct_x(model, z_seed)
-        plot(axes[0, 1], x_pred.squeeze().detach().cpu().numpy())
-        plot(axes[1, 1], x_pred.squeeze().detach().cpu().numpy())
-        for col_idx in range(2, N_COLS):
-            zc_sample, zs_sample = sample_prior(rng, model)
-            x_pred_causal = reconstruct_x(model, torch.hstack((zc_sample, zs_seed)))
-            x_pred_spurious = reconstruct_x(model, torch.hstack((zc_seed, zs_sample)))
+        for col_idx in range(1, N_COLS):
+            zc_sample, zs_sample, zx_sample = sample_prior(rng, model)
+            x_pred_causal = reconstruct_x(model, torch.hstack((zc_sample, zs_seed, zx_seed)))
+            x_pred_spurious = reconstruct_x(model, torch.hstack((zc_seed, zs_sample, zx_seed)))
+            x_pred_exogenous = reconstruct_x(model, torch.hstack((zc_seed, zs_seed, zx_sample)))
             plot(axes[0, col_idx], x_pred_causal.squeeze().detach().cpu().numpy())
             plot(axes[1, col_idx], x_pred_spurious.squeeze().detach().cpu().numpy())
+            plot(axes[2, col_idx], x_pred_exogenous.squeeze().detach().cpu().numpy())
         fig_dpath = os.path.join(task_dpath, f'version_{args.seed}', 'fig', 'plot_reconstruction')
         os.makedirs(fig_dpath, exist_ok=True)
         plt.savefig(os.path.join(fig_dpath, f'{example_idx}.png'))
