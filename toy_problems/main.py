2cb57f2c9a1b3391df5cbd05feac0a3516d40978
makinot1 <makinot1@gene.com>
2024-01-09 12:07:13 -0500

    Minor changes

--- toy_problems/main.py
+++ toy_problems/main.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     parser.add_argument('--h_sizes', nargs='+', type=int, default=[256, 256])
     parser.add_argument('--prior_reg_mult', type=float, default=1e-5)
     parser.add_argument('--init_sd', type=float, default=1)
-    parser.add_argument('--kl_anneal_epochs', type=int, default=20)
+    parser.add_argument('--kl_anneal_epochs', type=int, default=50)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--weight_decay', type=float, default=0.01)
     parser.add_argument('--n_epochs', type=int, default=100)
